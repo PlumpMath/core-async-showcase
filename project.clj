3e5e1b1f218d7400ddc60e3bb85a2240efd3b0bb
zacyang <yangmeansyoung@gmail.com>
2015-02-02 22:29:00 +0800

    add basic examples

--- project.clj
+++ project.clj
@@ -1,6 +1,10 @@
 (defproject core-async-showcase "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "core.async showcase for TW tech radar session"
+  :url "https://github.com/zacyang/core-async-showcase"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]])
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [org.clojure/core.async "0.1.346.0-17112a-alpha"]
+                 ]
+  :plugins [[cider/cider-nrepl "0.8.2"]]
+  )
--- src/core_async_showcase/core.clj
+++ src/core_async_showcase/core.clj
@@ -1,6 +1,23 @@
-(ns core-async-showcase.core)
+(ns core-async-showcase.core
+  (:require [clojure.core.async :refer :all :as async]) 
+  )
+;;; channel with default parameters
+(def c (chan))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(chan)
+
+(take! c (fn [v] (println v)))
+
+(put! c 42)
+
+(put! c "hehe")
+
+(take! c (fn [v] (println "GOT" v)))
+
+;;;;;;;;;;;;;;;;;;;;;
+;;fun started here
+;;;;;;;;;;;;;;;;;;;;
+
+(put! c "hello world" (fn [] (println "done putting")))
+
+(take! c #(println "GOT" %1))
